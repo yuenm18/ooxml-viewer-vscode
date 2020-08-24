185e647999ce315ff88a7c0d5d6d3b6e5e6a7a46
Mike Bowen <mibowe@microsoft.com>
2020-08-24 12:11:19 -0700

    delete temp files and close tabs when switching to different ooxml file

--- src/ooxml-viewer.ts
+++ src/ooxml-viewer.ts
@@ -1,4 +1,4 @@
-import { readFile, writeFile } from 'fs';
+import { readFile, writeFile, watch } from 'fs';
 import { join, parse, dirname } from 'path';
 import JSZip, { JSZipObject } from 'jszip';
 import format from 'xml-formatter';
@@ -7,9 +7,11 @@ import vscode, { Uri, TextDocument } from 'vscode';
 import mkdirp from 'mkdirp';
 import { promisify } from 'util';
 import { exec } from 'child_process';
+import rimraf from 'rimraf';
 const execPromise = promisify(exec);
 const readFilePromise = promisify(readFile);
 const writeFilePromise = promisify(writeFile);
+const rmrafPromise = promisify(rimraf);
 
 /**
  * The OOXML Viewer
@@ -17,10 +19,12 @@ const writeFilePromise = promisify(writeFile);
 export class OOXMLViewer {
     treeDataProvider: OOXMLTreeDataProvider;
     zip: JSZip;
+    fileCachePath: string;
 
     constructor() {
         this.treeDataProvider = new OOXMLTreeDataProvider();
         this.zip = new JSZip();
+        this.fileCachePath = join(vscode.workspace.rootPath ?? parse(process.cwd()).root, '.ooxml-temp-file-folder-78kIPsmTq5TK');
     }
 
     /**
@@ -34,6 +38,14 @@ export class OOXMLViewer {
             let data = await readFilePromise(file.fsPath);
             await this.zip.loadAsync(data);
             this.populateOOXMLViewer(this.zip.files);
+            // TODO: Use this watch to update the ooxml file when the file is changed from outside vscode. i.e. in PowerPoint
+            // watch(file.fsPath, { encoding: 'buffer' }, (eventType: string, filename: Buffer): void => {
+            //     vscode.window.showWarningMessage('I am a warning', { modal: true }, ...['Save', 'Discard']);
+            //     if (filename) {
+            //         console.log(filename);
+            //         // Prints: <Buffer ...>
+            //     }
+            // });
         } catch (e) {
             console.error(e);
             vscode.window.showErrorMessage(`Could not load ${file.fsPath}`, e);
@@ -50,18 +62,17 @@ export class OOXMLViewer {
             const file: JSZipObject | null = this.zip.file(fileNode.fullPath);
             const text: string = await file?.async('text') ?? '';
             const formattedXml: string = format(text);
-            const root: string = vscode.workspace.rootPath ?? parse(process.cwd()).root;
-            const folderPath = join(root, '.ooxml-temp-file-folder-78kIPsmTq5TK', dirname(fileNode.fullPath));
+            // const root: string = vscode.workspace.rootPath ?? parse(process.cwd()).root;
+            const folderPath = join(this.fileCachePath, dirname(fileNode.fullPath));
             const filePath: string = join(folderPath, fileNode.fileName);
             const created: string | void = await mkdirp(folderPath);
             // On Windows hide the folder
             if (process.platform.startsWith('win')) {
-                const { stdout, stderr } = await execPromise('attrib +h ' + join(root, '.ooxml-temp-file-folder-78kIPsmTq5TK'));
+                const { stdout, stderr } = await execPromise('attrib +h ' + this.fileCachePath);
                 if (stderr) {
                     throw new Error(stderr);
                 }
             }
-
             await writeFilePromise(filePath, formattedXml, 'utf8');
             const xmlDoc: TextDocument = await vscode.workspace.openTextDocument(Uri.parse("file:///" + filePath));
 
@@ -79,10 +90,37 @@ export class OOXMLViewer {
         this.resetOOXMLViewer();
     }
 
-    private resetOOXMLViewer() {
-        this.zip = new JSZip();
-        this.treeDataProvider.rootFileNode = new FileNode();
-        this.treeDataProvider.refresh();
+    private async resetOOXMLViewer() {
+        const closeDoc = (tds: TextDocument[], tempDirectoryName: string): void => {
+            if (tds.length) {
+                const td: TextDocument | undefined = tds.pop();
+                if (td) {
+                    vscode.window.showTextDocument(td, { preview: true, preserveFocus: false })
+                        .then(d => {
+                            if (td.fileName.includes(tempDirectoryName)) {
+                                return vscode.commands.executeCommand('workbench.action.closeActiveEditor');
+                            } else {
+                                return;
+                            }
+                        })
+                        .then(() => {
+                            closeDoc(tds, tempDirectoryName);
+                        });
+                }
+            }
+        };
+
+        try {
+            this.zip = new JSZip();
+            this.treeDataProvider.rootFileNode = new FileNode();
+            this.treeDataProvider.refresh();
+            await rmrafPromise(this.fileCachePath);
+            const textDocuments: TextDocument[] = [...vscode.workspace.textDocuments];
+            closeDoc(textDocuments, this.fileCachePath);
+        } catch (err) {
+            console.error(err);
+            vscode.window.showErrorMessage('Could not remove ooxml file viewer cache');
+        }
     }
 
     private populateOOXMLViewer(files: { [key: string]: JSZip.JSZipObject }) {
