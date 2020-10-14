3a3f78d7f850797c9ae12d42bac2b3a58eb17a20
Mike Bowen <mibowe@microsoft.com>
2020-10-14 14:58:47 -0700

    refactor imports

--- src/extension.ts
+++ src/extension.ts
@@ -1,18 +1,20 @@
 import rimraf from 'rimraf';
 import { promisify } from 'util';
-import * as vscode from 'vscode';
+import { commands, ExtensionContext, Uri, window } from 'vscode';
 import { FileNode } from './ooxml-tree-view-provider';
 import { OOXMLViewer } from './ooxml-viewer';
 
 const rimrafPromise = promisify(rimraf);
 
-export function activate(context: vscode.ExtensionContext): void {
+export function activate(context: ExtensionContext): void {
   const ooxmlViewer = new OOXMLViewer(context);
-  context.subscriptions.push(vscode.window.registerTreeDataProvider('ooxmlViewer', ooxmlViewer.treeDataProvider));
-  context.subscriptions.push(vscode.commands.registerCommand('ooxmlViewer.viewContents', async (file: vscode.Uri) => ooxmlViewer.viewContents(file)));
-  context.subscriptions.push(vscode.commands.registerCommand('ooxmlViewer.viewFile', async (fileNode: FileNode) => ooxmlViewer.viewFile(fileNode)));
-  context.subscriptions.push(vscode.commands.registerCommand('ooxmlViewer.clear', () => ooxmlViewer.clear()));
-  context.subscriptions.push(vscode.commands.registerCommand('ooxmlViewer.showDiff', async (file: FileNode) => ooxmlViewer.getDiff(file)));
+  context.subscriptions.push(window.registerTreeDataProvider('ooxmlViewer', ooxmlViewer.treeDataProvider));
+  context.subscriptions.push(commands.registerCommand('ooxmlViewer.viewContents', async (file: Uri) => ooxmlViewer.viewContents(file)));
+  context.subscriptions.push(
+    commands.registerCommand('ooxmlViewer.viewFile', async (fileNode: FileNode) => ooxmlViewer.viewFile(fileNode)),
+  );
+  context.subscriptions.push(commands.registerCommand('ooxmlViewer.clear', () => ooxmlViewer.clear()));
+  context.subscriptions.push(commands.registerCommand('ooxmlViewer.showDiff', async (file: FileNode) => ooxmlViewer.getDiff(file)));
 }
 
 export async function deactivate(): Promise<void> {
