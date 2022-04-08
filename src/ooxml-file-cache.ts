9b10f5deaea7e40664085930e11f6b3931229970
Michael Bowen <mikee.bowen@gmail.com>
2022-04-08 11:08:28 -0700

    don't check if file exists when in readFile

--- src/ooxml-file-cache.ts
+++ src/ooxml-file-cache.ts
@@ -322,11 +322,7 @@ export class OOXMLFileCache {
    */
   async readFile(cachedFilePath: string): Promise<Uint8Array> {
     try {
-      if (existsSync(cachedFilePath)) {
-        return await workspace.fs.readFile(Uri.file(cachedFilePath));
-      }
-
-      return new Uint8Array();
+      return await workspace.fs.readFile(Uri.file(cachedFilePath));
     } catch (err) {
       await ExtensionUtilities.handleError(err);
     }
