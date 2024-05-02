d602a90a65c0614c20e24c5c6f3eabbe578d8ee2
Matthew Yuen <11188600+yuenm18@users.noreply.github.com>
2024-05-02 04:55:07 +0000

    Update dependencies

--- test/suite/index.ts
+++ test/suite/index.ts
@@ -1,8 +1,8 @@
-import glob from 'glob';
+import { glob } from 'glob';
 import Mocha from 'mocha';
 import path from 'path';
 
-export function run(): Promise<void> {
+export async function run(): Promise<void> {
   // Create the mocha test
   const mocha = new Mocha({
     ui: 'tdd',
@@ -11,26 +11,19 @@ export function run(): Promise<void> {
 
   const testsRoot = path.resolve(__dirname, '..');
 
+  const files = await glob.glob('**/**.test.js', { cwd: testsRoot });
+
+  // Add files to the test suite
+  files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
+
+  // Run the mocha test
+
   return new Promise((c, e) => {
-    glob('**/**.test.js', { cwd: testsRoot }, (err, files) => {
-      if (err) {
-        return e(err);
-      }
-
-      // Add files to the test suite
-      files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
-
-      try {
-        // Run the mocha test
-        mocha.run(failures => {
-          if (failures > 0) {
-            e(new Error(`${failures} tests failed.`));
-          } else {
-            c();
-          }
-        });
-      } catch (err) {
-        e(err);
+    mocha.run(failures => {
+      if (failures > 0) {
+        e(new Error(`${failures} tests failed.`));
+      } else {
+        c();
       }
     });
   });
